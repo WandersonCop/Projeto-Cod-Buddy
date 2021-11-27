7647d2fca4264c8836c8bdeeaeb2497b0ca14912
FIRST_NAME LAST_NAME <wandersoncop@gmail.com>
2021-11-27 08:50:53 -0300

    dia 27/11

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ let Cadastro = {
   email: '',
   telefone: '',
   data_nascimento: '',
-  masculino: '',
-  feminino: '',
-  outro: '',
+  genero: '',
   cidade: '',
   estado: '',
-  endereco: '',
+  endereco: ''
 }
 function preencherObjeto() {
   Cadastro.nome = document.getElementById('nome').value
   Cadastro.email = document.getElementById('email').value
   Cadastro.telefone = document.getElementById('telefone').value
   Cadastro.data_nascimento = document.getElementById('data_nascimento').value
-  Cadastro.masculino = document.getElementById('masculino').value
-  Cadastro.feminino = document.getElementById('feminino').value
-  Cadastro.outro = document.getElementById('outro').value
+
+  // Cadastro.genero = document.getElementsByName('genero'){
+  //   var radios =
+  //   document.getElementsByName("genero");
+  // for (var i = 0; i < radios.length; i++) {
+  //     if (radios[i].checked) {
+  //         console.log( radios[i].value);
+  //     }
+  // }
+  
   Cadastro.cidade = document.getElementById('cidade').value
   Cadastro.estado = document.getElementById('estado').value
   Cadastro.endereco = document.getElementById('endereco').value
